84feb869c8360ee7bcdb94431d9f6974b5d041f6
Rostand Molo <kaserekamwiraros@gmail.com>
2023-08-22 19:44:36 +0100

    error fixed

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('update-profile/{id}',RegisteredUserController::class,'updateTwoo');
+Route::get('update-profile/{id}',[RegisteredUserController::class,'updateTwoo']);
 
-Route::get('update-photo/{id}',RegisteredUserController::class,'updateTwoo');
+Route::get('update-photo/{id}',[RegisteredUserController::class,'updateTwoo']);
